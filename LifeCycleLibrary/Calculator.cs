f82e56df634ac6f8f743a511b525aa9c39cc596b
Juan Pablo Guerra <jguer168@gmail.com>
2021-12-31 12:55:10 -0500

    Add Class Library Project
    Adds the class library project with the SQL load
    class and the calculator class for future use.

---
+++ LifeCycleLibrary/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeCycleLibrary
+{
+    public static class Calculator
+    {
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ LifeCycleLibrary/SimpleDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace LifeCycleLibrary
+{
+    public class SimpleDataAccess
+    {
+        private static string GetConenctionString(string name = "UserDatabase")
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public static List<PersonModel> LoadPeople() 
+        {
+            string sql = "select * from dbo.Person";
+            using (IDbConnection cnn = new SqlConnection(GetConenctionString()))
+            {
+                var people = cnn.Query<PersonModel>(sql).ToList();
+                return people; 
+            }
+        }
+
+
+    }
+}
