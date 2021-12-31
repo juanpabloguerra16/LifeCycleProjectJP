30d1bbf23eadbfddc883ea50a9f39e0241b526ef
Juan Pablo Guerra <jguer168@gmail.com>
2021-12-31 11:43:46 -0500

    Initial Template Creation
    Added the ASP.NET MVC Templete with individual
    authentication enabled.

---
+++ MVCLifeCyple/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLifeCyple.Startup))]
+namespace MVCLifeCyple
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
