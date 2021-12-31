ced76b876bae9556021f3040125b30f466c0e9c1
Juan Pablo Guerra <jguer168@gmail.com>
2021-12-31 15:29:58 -0500

    Add Unit Test Project
    Adds the XUnit test project and configures one initial test

---
+++ LifeCycleLibrary.Test/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LifeCycleLibrary.Test
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(4, 2, 2)]
+        public void Divide_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            // Arrange
+
+
+            // Act
+            double actual = Calculator.Divide(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
